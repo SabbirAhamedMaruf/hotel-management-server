cc62f0a19bc6b0c71cb086d74cf4c5d2b48697fe
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2023-11-07 19:07:48 +0600

    added api for featured products

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
-app.use(cors({origin:["http://localhost:5173"],credentials:true}));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Mongodb config
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.ILK_LODGE_USER}:${process.env.ILK_LODGE_PASS}@cluster0.pzharqa.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,78 +28,95 @@ async function run() {
     // database
     const database = client.db("Ilk_Lodge");
     const homePageReviewCollection = database.collection("HomePageReview");
-    const roomsCollection  = database.collection("rooms");
+    const roomsCollection = database.collection("rooms");
 
 
-
+    
     // jwt token generation
-    app.post("/jwt",async(req,res)=>{
+    app.post("/jwt", async (req, res) => {
       const payload = req.body;
-      const token = jwt.sign(payload,process.env.AUTHORIZATION_TOKEN,{expiresIn:"1h"});
-      res.cookie("token",token).send({"success" : true})
-    })
-
+      const token = jwt.sign(payload, process.env.AUTHORIZATION_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token).send({ success: true });
+    });
 
 
 
     // Home page hotel testimonials
-    app.get("/testimonial",async(req,res)=>{
+    app.get("/testimonial", async (req, res) => {
       const testimonialData = await homePageReviewCollection.find().toArray();
-      res.send(testimonialData)
-    })
+      res.send(testimonialData);
+    });
+
+
 
 
     // Get single room data for single room detail page
-    app.get("/rooms/singleRoomDetails/:id",async(req,res)=>{
+    app.get("/rooms/singleRoomDetails/:id", async (req, res) => {
       const currentRoomId = req.params.id;
       const query = { _id: new ObjectId(currentRoomId) };
       const resut = await roomsCollection.findOne(query);
       res.send(resut);
-    })
+    });
 
 
 
-
-
-    // Fetching room data 
-    app.get("/rooms",async(req,res)=>{
+    // Fetching room data
+    app.get("/rooms", async (req, res) => {
       const sortData = req.query.sortByHighToLow;
-      console.log(sortData);
-      const options ={
-        projection : {_id:1, photo:1, price:1}
+      const options = {
+        projection: { _id: 1, photo: 1, price: 1, reviewCount: 1 },
+      };
+      if (sortData === "true") {
+        const result = await roomsCollection
+          .find({}, options)
+          .sort({ price: -1 })
+          .toArray();
+        res.send(result);
+      } else {
+        const result = await roomsCollection
+          .find({}, options)
+          .sort({ price: 1 })
+          .toArray();
+        res.send(result);
       }
-      if(sortData === "true"){
-         const result = await roomsCollection.find({},options).sort({price: -1}).toArray()
-         res.send(result)
-      }else{
-        const result = await roomsCollection.find({},options).sort({price: 1}).toArray();
-        res.send(result)
-      }
-    })
+    });
 
 
 
 
-
-
-
-
+    // Fetching data for featured rooms
+    app.get("/featuredProduct", async (req, res) => {
+      const query = { featured: "true" };
+      const options = {
+        projection: { _id: 1, photo: 1, price: 1 },
+      };
+      const result = await roomsCollection.find(query, options).toArray();
+      res.send(result);
+    });
 
 
 
 
 
     // add rooms from add room page
-    app.post("/addroom",async(req,res)=>{
+    app.post("/addroom", async (req, res) => {
       const currentRoomData = req.body;
       const result = await roomsCollection.insertOne(currentRoomData);
       res.send(result);
-    })
+    });
 
 
 
-    
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -107,12 +124,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 // Server config
-app.get("/",(req,res)=>{
-    res.send("Server is running!");
-})
-app.listen(port,()=>{
-    console.log("Server is running on port",port);
-})
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
