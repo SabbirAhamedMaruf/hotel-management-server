6636090f168d2cf178bce418f69b6b8810ed58a3
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2023-11-08 23:22:49 +0600

    added live site link on cors origin policy. Final commit

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 
 // Custom middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://654bbeed40b0ee1727cfa53b--chimerical-treacle-35dd40.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
@@ -46,14 +53,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
     // database
     const database = client.db("Ilk_Lodge");
     const homePageReviewCollection = database.collection("HomePageReview");
     const roomsCollection = database.collection("rooms");
     const bookedRoomsCollection = database.collection("bookedRooms");
     const userReviewCollection = database.collection("userReview");
-
 
     // jwt token generation
     app.post("/jwt", async (req, res) => {
@@ -70,7 +75,6 @@ async function run() {
         .send({ success: true });
     });
 
-
     // Removing cookie when user logged out
     app.post("/clearuserjwttoken", async (req, res) => {
       // here we getting user information that which user is logged in and out.
@@ -78,13 +82,11 @@ async function run() {
       res.clearCookie("token", { maxAge: 0 }).send({ success: true });
     });
 
-
     // Home page hotel testimonials
     app.get("/testimonial", async (req, res) => {
       const testimonialData = await homePageReviewCollection.find().toArray();
       res.send(testimonialData);
     });
-
 
     // Get single room data for single room detail page
     app.get("/rooms/singleRoomDetails/:id", async (req, res) => {
@@ -99,37 +101,30 @@ async function run() {
     // Fetching room data
     app.get("/rooms", async (req, res) => {
       const sortData = req.query.sortRooms;
-      AvailableQuery = {available : true}
+      AvailableQuery = { available: true };
       const options = {
         projection: { _id: 1, photo: 1, price: 1, reviewCount: 1 },
       };
-      if(sortData === "high"){
-          const result = await roomsCollection
+      if (sortData === "high") {
+        const result = await roomsCollection
           .find({}, options)
           .sort({ price: -1 })
           .toArray();
-          res.send(result);
-      }
-      else if(sortData === "low") {
+        res.send(result);
+      } else if (sortData === "low") {
         const result = await roomsCollection
           .find({}, options)
           .sort({ price: 1 })
           .toArray();
         res.send(result);
-      }
-      else if(sortData === "availableroom"){
-        const result = await roomsCollection
-          .find(AvailableQuery)
-          .toArray();
+      } else if (sortData === "availableroom") {
+        const result = await roomsCollection.find(AvailableQuery).toArray();
         res.send(result);
-      }else{
-        const result = await roomsCollection
-          .find()
-          .toArray();
+      } else {
+        const result = await roomsCollection.find().toArray();
         res.send(result);
       }
     });
-
 
     // Fetching data for featured rooms
     app.get("/featuredProduct", async (req, res) => {
@@ -141,17 +136,13 @@ async function run() {
       res.send(result);
     });
 
-
-  
     // User booked rooms data for mybooking page
-    app.post("/mybookings",verifyToken, async (req, res) => {
-
-      console.log("User token",req.user);
-      if(req.user.email === req.body.userEmail){
+    app.post("/mybookings", verifyToken, async (req, res) => {
+      console.log("User token", req.user);
+      if (req.user.email === req.body.userEmail) {
         const currentUserEmail = req.body.userEmail;
         console.log(currentUserEmail);
-  
-  
+
         const query = { userEmail: currentUserEmail };
         const options = {
           projection: { currentRoomData: 1, _id: 1 },
@@ -160,103 +151,87 @@ async function run() {
           .find(query, options)
           .toArray();
         res.send(currentUserBookings);
-      }else{
-        return res.status(403).send({message: 'forbidden access'})
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
       }
-     
     });
 
-
-
-
-
-
-
-
-
-
-
     // add rooms from add room page
-    app.post("/addroom",verifyToken, async (req, res) => {
+    app.post("/addroom", verifyToken, async (req, res) => {
       console.log(req.query);
-      if(req.user.email === req.query.userEmail){
+      if (req.user.email === req.query.userEmail) {
         const currentRoomData = req.body;
         console.log(currentRoomData);
         const result = await roomsCollection.insertOne(currentRoomData);
         res.send(result);
-      }else{
-        return res.status(403).send({message: 'forbidden access'})
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      
     });
-
 
     // adding review text for review field
-    app.patch("/addreview",verifyToken, async (req, res) => {
-      if(req.user.email === req.query.userEmail){
+    app.patch("/addreview", verifyToken, async (req, res) => {
+      if (req.user.email === req.query.userEmail) {
         const userReview = req.body;
-      const roomUpdateId = { _id: new ObjectId(userReview.id) };
-      const roomDataFromRoomCollection = await roomsCollection.findOne(
-        roomUpdateId
-      );
-      const updateCount = roomDataFromRoomCollection.reviewCount;
-      const currentReviewCount = updateCount + 1;
-      const updateDoc = {
-        $set: {
-          reviewCount: currentReviewCount,
-        },
-      };
-      const reviewCountUpdate = await roomsCollection.updateOne(
-        roomUpdateId,
-        updateDoc
-      );
-      const result = await userReviewCollection.insertOne(userReview);
-      res.send(result);
-      }else{
-        return res.status(403).send({message: 'forbidden access'})
-      }
-    });
-
-
-
-
-    app.patch("/rooms/singleRoomDetails/bookRoom",verifyToken, async (req, res) => {
-      if(req.user.email === req.query.user){
-        const userEmail = req.query.user;
-        const currentRoomData = req.body.bookingData;
-  
-        // Updating availablity state
-        const userBookedRoomId = currentRoomData.roomId;
-        const bookedDate = currentRoomData.date;
-        console.log(bookedDate);
-        const query = { _id: new ObjectId(userBookedRoomId) };
-  
+        const roomUpdateId = { _id: new ObjectId(userReview.id) };
+        const roomDataFromRoomCollection = await roomsCollection.findOne(
+          roomUpdateId
+        );
+        const updateCount = roomDataFromRoomCollection.reviewCount;
+        const currentReviewCount = updateCount + 1;
         const updateDoc = {
           $set: {
-            available: false,
-            lastbookDate: bookedDate,
+            reviewCount: currentReviewCount,
           },
         };
-        const roomDataFromRoomCollection = await roomsCollection.updateOne(
-          query,
+        const reviewCountUpdate = await roomsCollection.updateOne(
+          roomUpdateId,
           updateDoc
         );
-        // Inset booked data on bookedRooms collection
-        const data = { userEmail, currentRoomData };
-        const result = await bookedRoomsCollection.insertOne(data);
+        const result = await userReviewCollection.insertOne(userReview);
         res.send(result);
-      }else{
-        return res.status(403).send({message: 'forbidden access'})
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
       }
     });
 
+    app.patch(
+      "/rooms/singleRoomDetails/bookRoom",
+      verifyToken,
+      async (req, res) => {
+        if (req.user.email === req.query.user) {
+          const userEmail = req.query.user;
+          const currentRoomData = req.body.bookingData;
 
+          // Updating availablity state
+          const userBookedRoomId = currentRoomData.roomId;
+          const bookedDate = currentRoomData.date;
+          console.log(bookedDate);
+          const query = { _id: new ObjectId(userBookedRoomId) };
 
-    // >Need to apply jwt token verification
+          const updateDoc = {
+            $set: {
+              available: false,
+              lastbookDate: bookedDate,
+            },
+          };
+          const roomDataFromRoomCollection = await roomsCollection.updateOne(
+            query,
+            updateDoc
+          );
+          // Inset booked data on bookedRooms collection
+          const data = { userEmail, currentRoomData };
+          const result = await bookedRoomsCollection.insertOne(data);
+          res.send(result);
+        } else {
+          return res.status(403).send({ message: "forbidden access" });
+        }
+      }
+    );
 
     // booked room update date
-    app.patch("/updateBookedDate",verifyToken, async (req, res) => {
-      if(req.user.email === req.query.userEmail){
+    app.patch("/updateBookedDate", verifyToken, async (req, res) => {
+      if (req.user.email === req.query.userEmail) {
         const { newDate, bookedId, roomId } = req.query;
         const roomDataQuery = { _id: new ObjectId(roomId) };
         const roomDataUpdate = {
@@ -268,7 +243,7 @@ async function run() {
           roomDataQuery,
           roomDataUpdate
         );
-  
+
         const bookedRecordQuery = { _id: new ObjectId(bookedId) };
         const bookedRoomDataUpdate = {
           $set: {
@@ -280,39 +255,35 @@ async function run() {
           bookedRoomDataUpdate
         );
         res.send(bookedRecordData);
-      }else{
-        return res.status(403).send({message: 'forbidden access'})
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      
-    
     });
-
 
     // Booked Room Delete api
 
-    app.delete("/deletebookedroom",verifyToken, async (req, res) => {
-      if(req.user.email === req.query.userEmail){
+    app.delete("/deletebookedroom", verifyToken, async (req, res) => {
+      if (req.user.email === req.query.userEmail) {
         const currentDeletionDataId = req.query.id;
-      const roomDetailId = req.query.roomId;
-      const query = { _id: new ObjectId(roomDetailId) };
-      const updateDoc = {
-        $set: {
-          available: true,
-          lastbookDate: "",
-        },
-      };
-      const roomDataFromRoomCollection = await roomsCollection.updateOne(
-        query,
-        updateDoc
-      );
-      console.log(roomDataFromRoomCollection);
-      const deleteQuery = { _id: new ObjectId(currentDeletionDataId) };
-      const result = await bookedRoomsCollection.deleteOne(deleteQuery);
-      res.send(result);
-      }else{
-        return res.status(403).send({message: 'forbidden access'})
+        const roomDetailId = req.query.roomId;
+        const query = { _id: new ObjectId(roomDetailId) };
+        const updateDoc = {
+          $set: {
+            available: true,
+            lastbookDate: "",
+          },
+        };
+        const roomDataFromRoomCollection = await roomsCollection.updateOne(
+          query,
+          updateDoc
+        );
+        console.log(roomDataFromRoomCollection);
+        const deleteQuery = { _id: new ObjectId(currentDeletionDataId) };
+        const result = await bookedRoomsCollection.deleteOne(deleteQuery);
+        res.send(result);
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      
     });
 
     console.log(
